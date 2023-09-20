69921b7a7bba28be9f351a0d294e586bd1787b5c
Leonardo Neves <Leonardo.Neves1@ibm.com>
2023-09-20 16:30:15 -0300

    Atualizacao do sobre

--- components/Navigation/TheHeader.vue
+++ components/Navigation/TheHeader.vue
@@ -9,7 +9,7 @@
     <div class="navigation-items">
       <ul class="nav-list">
         <li class="nav-item"><nuxt-link to="/posts">Blog</nuxt-link></li>
-        <li class="nav-item"><nuxt-link to="/about">About</nuxt-link></li>
+        <li class="nav-item"><nuxt-link to="/about">Sobre nós</nuxt-link></li>
         <li class="nav-item"><nuxt-link to="/admin">Admin</nuxt-link></li>
       </ul>
     </div>
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="about-page">
-    <h1>Sobre o autor e sobre o blog</h1>
-    <p>Este Blog foi desenvolvido utilizando o framework Nuxt JS que por sua vez é uma variação do Vue JS</p>
+    <h1>Sobre o autor e sobre o blog:</h1>
+    <p>Este Blog foi desenvolvido </p>
+    <p>como parte do projeto integrador para o curso de Engenharia da Computação da UNIVESP - Polo Guarulhos</p>
+    <p>pelos alunos:</p>
+    <p>Emerson Nunes Barbosa, 1823884</p>
+    <p>Fabio Camilo, 2110014</p>
+    <p>Leví Anjos de Souza, 2110035</p>
+    <p>Leonardo Vinicius Neves, 1701289</p>
+    <p>Marcos Soares Alves, 1835191</p>
+    <p></p>
+    <p>Este Blog foi desenvolvido utilizando o framework Nuxt JS que por sua vez é uma variação do Vue JS.</p>
+    <p>Sua API está contida no Modulo Server da aplicação também utilizando O Nuxt JS,</p>
+    <p>e seu banco de dados não realcional está armazenado no serviço firebase da Google.</p>
+    <p>A imagem da aplicação está sendo executada no serviço Cloud engine da nuvem da IBM. </p>
+    <p>O codigo fonte está sendo controlado pelo versionamento do GitHub</p>
   </div>
 </template>
 
